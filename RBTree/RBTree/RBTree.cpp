8875b544c3504db58572cd77f7567b257df20296
Tong <77432977+ZhT515@users.noreply.github.com>
2021-12-11 15:44:51 +0800

    Add files via upload

--- RBTree/RBTree/RBTree.cpp
+++ RBTree/RBTree/RBTree.cpp
@@ -1,6 +1,27 @@
 #include"RBTree.h"
+#include <stdlib.h>
+#include <time.h>
+
+void TestRBTree()
+{
+	RBTree<int, int> t;
+	int n = 10000;
+	srand(time(0));
+	for (int i = 0; i < n; ++i)
+	{
+		int e = rand();
+		t.Insert(make_pair(e, e));
+	}
+
+	//t.InOrder();
+
+	cout << t.CheckBlance() << endl;
+	
+}
 
 int main()
 {
+	TestRBTree();
 
+	return 0;
 }
